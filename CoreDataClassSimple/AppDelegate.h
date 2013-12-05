58118095484d9a8c8749a2a4d48a9b61f8417bc0
lzhr <lzhr@lizhongrentekiMacBook-Air.local>
2013-12-05 09:43:23 +0800

    Initial Commit

---
+++ CoreDataClassSimple/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataClassSimple
+//
+//  Created by lzhr on 13-12-5.
+//  Copyright (c) 2013年 lizhongren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoreDataClassSimple/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataClassSimple
+//
+//  Created by lzhr on 13-12-5.
+//  Copyright (c) 2013年 lizhongren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
